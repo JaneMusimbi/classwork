dbbc39ea3af25fc4e0107e1f7a49b327d6732526
JaneMusimbi <janemusimbi@gmail.com>
2022-02-23 18:35:02 -0500

    revision2

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    Modulus(15,6)
+    person("jane" ,"musimbi")
+    var y= addition(3,5,8,9)
+    println(y)
+}
+
+fun Modulus(num1:Int, num2:Int){
+    var x=(num1%num2)
+    println(x)
+}
+fun person(name1:String,name2:String){
+    var x=("jane musimbi")
+    println(x)
+}
+
+fun addition(a:Int,b:Int,c:Int,d:Int):Int{
+    var sum=(a+b+c+d)
+    return sum
+}
+
+
+
+
+
